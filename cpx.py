3d8e170f52cd251f2a21543d5c9cdff13e46571a
VTITorhout <85348797+VTITorhout@users.noreply.github.com>
2025-01-08 20:51:19 +0100

    Major update
    Hfdst 7 grotendeel afgewerkt

--- cpx.py
+++ cpx.py
@@ -1,4 +1,6 @@
+import math
 def car2pol(a,b):
     #convert carthesian (a+j.b) to polair (mod/arg)
-    car = complex(f"{a}+{b}j")
-    return abs(car)
+    mod = abs(complex((f"{a}+{b}j")))
+    arg = math.degrees(math.atan(b/a))
+    return mod,arg
